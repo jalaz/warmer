6aadc729dd3a9dc86b523965cc7c23f42e02fef7
Oleksii <azaalexey@gmail.com>
2017-01-31 00:13:41 -0800

    init + testing device simulation

---
+++ DeviceSimulator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.Devices.Client;
+
+namespace DeviceSimulator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                            .AddUserSecrets("Warmer.DeviceSimulator")
+                            .Build();
+
+            Run(config).Wait();
+            Console.WriteLine("message is successfuly sent");
+        }
+
+        public static async Task Run(IConfigurationRoot config)
+        {
+            var connectionString = config["WarmerConnectionString"];
+            var client = DeviceClient.CreateFromConnectionString(connectionString);
+
+            var content = new Message(Encoding.UTF8.GetBytes("testing..."));
+            await client.SendEventAsync(content);
+        }
+    }
+}
