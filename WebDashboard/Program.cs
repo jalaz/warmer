666f0a97811ae969b252368222ab29f8372cef74
Oleksii <azaalexey@gmail.com>
2017-02-02 22:11:30 -0800

    setup web server

---
+++ WebDashboard/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddUserSecrets()
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .ConfigureLogging(l => l.AddConsole(config.GetSection("Logging")))
+                .ConfigureServices(s => s.AddRouting())
+                .Configure(app => {
+                    app.UseRouter(r => {
+                        r.MapGet("", async (request, response, routeData) => {
+                            response.ContentType = "text/html";
+                            await response.WriteAsync(File.ReadAllText("index.html"));
+                        });
+                    });
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
