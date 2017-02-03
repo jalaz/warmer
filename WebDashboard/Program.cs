24d988385e3f2c149fd416f81c1927ad2c4f810a
Oleksii <azaalexey@gmail.com>
2017-02-02 23:01:47 -0800

    add SignalR hub that publishes random values

--- WebDashboard/Program.cs
+++ WebDashboard/Program.cs
@@ -1,11 +1,14 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.SignalR.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace ConsoleApplication
 {
@@ -25,14 +28,32 @@ namespace ConsoleApplication
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .ConfigureLogging(l => l.AddConsole(config.GetSection("Logging")))
-                .ConfigureServices(s => s.AddRouting())
+                .ConfigureServices(s => {
+                    s.AddSignalR(options => {
+                        options.Hubs.EnableDetailedErrors = true;
+                    });
+                    s.AddRouting();
+                })
                 .Configure(app => {
                     app.UseRouter(r => {
                         r.MapGet("", async (request, response, routeData) => {
                             response.ContentType = "text/html";
                             await response.WriteAsync(File.ReadAllText("index.html"));
                         });
+
+                        r.MapGet("trigger", async (request, response, routeData) => {
+                            var connectionManager = (IConnectionManager)r.ServiceProvider.GetService(typeof(IConnectionManager));
+                            var random = new Random();
+                            
+                            while(true){
+                                await Task.Delay(1000);
+                                connectionManager.GetHubContext<DeviceHub>().Clients.All.publishTemperature($"{{\"temperature\" : \"{random.Next(0, 100)}\"}}");
+                            }
+                        });
                     });
+
+                    app.UseWebSockets();
+                    app.UseSignalR();
                 })
                 .Build();
 
