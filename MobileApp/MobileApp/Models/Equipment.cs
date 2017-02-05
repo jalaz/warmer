aef2ae7a633b0903916e712c131dada1fc0c6770
Aleksandr <litvinov.rd@gmail.com>
2017-02-05 19:14:05 +0200

    Xamarin. switch cell button

---
+++ MobileApp/MobileApp/Models/Equipment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MobileApp.Models
+{
+    public class Equipment
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsOn { get; set; }  
+    }
+}
